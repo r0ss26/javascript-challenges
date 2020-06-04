b552eb7a566f909fb996bcf90b474efc9ebc1f70
Ross Baker <rossbaker32@gmail.com>
2020-06-04 13:54:14 +1000

    Implement a function that gets a pokemons stats

--- api_challenges/script.js
+++ api_challenges/script.js
@@ -6,7 +6,24 @@ function makeGetRequest(url, callback) {
   request.onload = (res) => callback(res.currentTarget.responseText)
 }
 
-makeGetRequest('https://pokeapi.co/api/v2/pokemon/ditto', (data) => {
-  data = JSON.parse(data)
-  console.log(data)
-})
+// makeGetRequest('https://pokeapi.co/api/v2/pokemon/ditto', (data) => {
+//   data = JSON.parse(data)
+//   console.log(data)
+// })
+
+function getPokemonStats(pokemonName, callback) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    request.onload = (response) => {
+      if (request.status >= 200 && request.status < 400) {
+        let stats = JSON.parse(response.currentTarget.responseText).stats
+        callback(stats)
+      } else {
+        console.log('No pokemon data found')
+      }
+    }
+    request.send()
+}
+
+
+getPokemonStats('pikachu', data => console.log(data))
