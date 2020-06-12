6fda293870a096332289cc68c2f9dfb5b4260b4b
Ross Baker <rossbaker32@gmail.com>
2020-06-12 17:37:01 +1000

    Refactor using asnyc/await

--- api-app/script.js
+++ api-app/script.js
@@ -1,84 +1,57 @@
-// let request = new XMLHttpRequest();
-// request.open('get', 'https://swapi.dev/api/people/1/');
-// request.onload = (progressEvent) => {
-//   console.log(progressEvent);
+async function getNumberOfTheDay() {
+  // Get todays number from API then process it into JSON/
+  let response = await fetch('https://api.math.tools/numbers/nod');
+  let responseJSON = await response.json()
+  console.log(responseJSON);
   
-//   console.log(`Status Code: ${progressEvent.currentTarget.status}, Status Text: ${progressEvent.currentTarget.statusText}`)
-//   console.log(JSON.parse(progressEvent.currentTarget.responseText));
-//   document.write(progressEvent.currentTarget.responseText)
-//   let request2 = new XMLHttpRequest();
-//   request2.open('get', 'https://swapi.dev/api/people/2/');
-//   request2.onload = progressEvent => {
-//     document.write(progressEvent.currentTarget.responseText);
-//   }
-//   request2.send();
-// }
-// request.onerror = () => {
-//   console.log('A network error occured');
-// }
-// request.onprogress = (event) => {
-//   if (event.lengthComputable) console.log(`${event.loaded} / ${event.total}`)
-// }
-// request.send();
+  let numberOfTheDay = responseJSON.contents.nod.numbers;
 
+  // Render the decimal number and related facts/
+  displayNumberOfTheDay(numberOfTheDay.number)
+  displayNumberOfTheDayFacts(numberOfTheDay['general-facts'])
 
-function getNumberOfTheDay() {
-  fetch('https://api.math.tools/numbers/nod')
-    .then(response => {
-      return response.json();
-    })
-    .then(response => {
-      let numberOfTheDay = response.contents.nod.numbers;
-      console.log(numberOfTheDay);
-      
-      displayNumberOfTheDay(numberOfTheDay.number)
-      displayNumberOfTheDayFacts(numberOfTheDay['general-facts'])
+  // Event Listeners for displaying different number formats.
+  document.querySelector('#decimalConversion').addEventListener('click', () => {
+    displayNumberOfTheDay(numberOfTheDay.number)
+  })
 
-      document.querySelector('#decimalConversion').addEventListener('click', () => {
-        displayNumberOfTheDay(numberOfTheDay.number)
-      })
+  document.querySelector('#binaryConversion').addEventListener('click', () => {
+    displayNumberOfTheDay(numberOfTheDay.bases.binary.value)
+  })
 
-      document.querySelector('#binaryConversion').addEventListener('click', () => {
-        displayNumberOfTheDay(numberOfTheDay.bases.binary.value)
-      })
+  document.querySelector('#hexConversion').addEventListener('click', () => {
+    displayNumberOfTheDay(numberOfTheDay.bases.hexadecimal.value)
+  })
 
-      document.querySelector('#hexConversion').addEventListener('click', () => {
-        displayNumberOfTheDay(numberOfTheDay.bases.hexadecimal.value)
-      })
+  document.querySelector('#octalConversion').addEventListener('click', () => {
+    displayNumberOfTheDay(numberOfTheDay.bases.octal.value)
+  })
 
-      document.querySelector('#octalConversion').addEventListener('click', () => {
-        displayNumberOfTheDay(numberOfTheDay.bases.octal.value)
-      })
+  document.querySelector('#romanNumeral').addEventListener('click', () => {
+    displayNumberOfTheDay(numberOfTheDay.numerals.roman.display)
+  })
 
-      document.querySelector('#romanNumeral').addEventListener('click', () => {
-        displayNumberOfTheDay(numberOfTheDay.numerals.roman.display)
-      })
+  document.querySelector('#babylonianNumeral').addEventListener('click', () => {
+    displayNumberOfTheDay(numberOfTheDay.numerals.babylonian.display)
+  })
 
-      document.querySelector('#babylonianNumeral').addEventListener('click', () => {
-        displayNumberOfTheDay(numberOfTheDay.numerals.babylonian.display)
-      })
+  document.querySelector('#chineseNumeral').addEventListener('click', () => {
+    displayNumberOfTheDay(numberOfTheDay.numerals.chinese.display)
+  })
 
-      document.querySelector('#chineseNumeral').addEventListener('click', () => {
-        displayNumberOfTheDay(numberOfTheDay.numerals.chinese.display)
-      })
+  document.querySelector('#numberOfDigits').addEventListener('click', () => {
+    console.log(numberOfTheDay.recreational.noofdigits.display);
+    
+    displayNumberOfTheDay(numberOfTheDay.recreational.noofdigits.display)
+  })
 
-      document.querySelector('#numberOfDigits').addEventListener('click', () => {
-        console.log(numberOfTheDay.recreational.noofdigits.display);
-        
-        displayNumberOfTheDay(numberOfTheDay.recreational.noofdigits.display)
-      })
+  document.querySelector('#sumOfDigits').addEventListener('click', () => {
+    displayNumberOfTheDay(numberOfTheDay.recreational.digitssum.display)
+  })
 
-      document.querySelector('#sumOfDigits').addEventListener('click', () => {
-        displayNumberOfTheDay(numberOfTheDay.recreational.digitssum.display)
-      })
-
-      document.querySelector('#reverseDigits').addEventListener('click', () => {
-        displayNumberOfTheDay(numberOfTheDay.recreational.reverse.display)
-      })
-
-
-    })
-    // .catch(error => console.log(error));
+  document.querySelector('#reverseDigits').addEventListener('click', () => {
+    displayNumberOfTheDay(numberOfTheDay.recreational.reverse.display)
+  })
 }
 
 function displayNumberOfTheDay(number) {
